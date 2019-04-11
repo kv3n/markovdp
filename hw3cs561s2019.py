51bee1e261f75e96deaa7a96121b62a55b7dd168
Kishore Venkateshan <kishorevenkateshan@Kishores-MacBook-Pro.local>
2019-04-10 22:27:06 -0700

    Refactored to new architecture

--- hw3cs561s2019.py
+++ hw3cs561s2019.py
@@ -2,244 +2,230 @@ import time
 import random
 
 
-class Config:
-    def __init__(self, input_file):
-        with open(input_file + '.txt', 'rU') as fp:
-            lines = fp.readlines()
+class Treasure:
+    def __init__(self, row, col, value, prob, other_prob):
+        self.row = row
+        self.col = col
+        self.policy = 'E'
+        self.utility = {
+            False: [prob * value, other_prob * value],
+            True: [prob * value, other_prob * value]
+        }
 
-            index = 0
-            self.GridSize = int(lines[index])
+
+class Wall:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.policy = 'N'
+
+
+class Location:
+    def __init__(self, row, col, prob, other_prob, movement_reward, discount):
+        self.row = row
+        self.col = col
+
+        self.prob = prob
+        self.other_prob = other_prob
+        self.movement_reward = movement_reward
+        self.discount = discount
+
+        self.actions = []
+        self.actions.append([None, None, None])  # 'U'
+        self.actions.append([None, None, None])  # 'D'
+        self.actions.append([None, None, None])  # 'L'
+        self.actions.append([None, None, None])  # 'R'
+
+        self.utility = {
+            False: [prob * 0.0, other_prob * 0.0],
+            True: [prob * 0.0, other_prob * 0.0]
+        }
+        self.policy = 'N'
+
+    def do_update(self, bit, anti):
+        a = self.actions
+
+        utilities = [(a[0][0][bit][0] + a[0][1][bit][1] + a[0][2][bit][1], 'U'),
+                     (a[1][0][bit][0] + a[1][1][bit][1] + a[1][2][bit][1], 'D'),
+                     (a[2][0][bit][0] + a[2][1][bit][1] + a[2][2][bit][1], 'L'),
+                     (a[3][0][bit][0] + a[3][1][bit][1] + a[3][2][bit][1], 'R')]
+
+        max_utility, self.policy = max(utilities)
+        max_utility = self.movement_reward + self.discount * max_utility
+
+        self.utility[anti][0] = self.prob * max_utility
+        self.utility[anti][1] = self.other_prob * max_utility
+
+
+def read_file(input_file):
+    cashes = dict()
+    walls = dict()
+
+    with open(input_file + '.txt', 'rU') as fp:
+        lines = fp.readlines()
+
+        index = 0
+        grid_size = int(lines[index])
+        index += 1
+
+        # Process Walls
+        end_index = index + int(lines[index])
+        index += 1
+        while index <= end_index:
+            row_col = lines[index].split(',')
+            row = int(row_col[0]) - 1
+            col = int(row_col[1]) - 1
+
+            walls[(row, col)] = Wall(row, col)
             index += 1
 
-            self.BlockOfInterest = set()
-            self.CashGrids = dict()
+        # Process Cash Grids
+        end_index = index + int(lines[index])
+        index += 1
 
-            # Process Walls
-            end_index = index + int(lines[index])
-            index += 1
-            while index <= end_index:
-                row_col = lines[index].split(',')
-                row = int(row_col[0]) - 1
-                col = int(row_col[1]) - 1
+        # get prob and other prob
+        prob = float(lines[end_index + 1])
+        other_prob = (1.0 - prob) * 0.5  # Two other actions possible
 
-                self.BlockOfInterest.add((row, col))
-                index += 1
+        while index <= end_index:
+            row_col_reward = lines[index].split(',')
+            row = int(row_col_reward[0]) - 1
+            col = int(row_col_reward[1]) - 1
 
-            # Process Cash Grids
-            end_index = index + int(lines[index])
-            index += 1
-            while index <= end_index:
-                row_col_reward = lines[index].split(',')
-                row = int(row_col_reward[0]) - 1
-                col = int(row_col_reward[1]) - 1
-
-                self.BlockOfInterest.add((row, col))
-                self.CashGrids[(row, col)] = float(row_col_reward[2])
-                index += 1
-
-            # Get Probability of Move
-            self.ProbabilityOfMove = float(lines[index])
-            self.ProbabilityOfOtherMove = (1.0 - self.ProbabilityOfMove) * 0.5  # Two other actions possible
+            cashes[(row, col)] = Treasure(row, col, float(row_col_reward[2]), prob, other_prob)
             index += 1
 
-            # Get Movement Reward
-            self.MovementReward = float(lines[index])
-            index += 1
+        # We already fetched prob and other prob. So move iterator
+        index += 1
 
-            # Get Discount
-            self.Discount = float(lines[index])
-            index += 1
+        # Get Movement Reward
+        reward = float(lines[index])
+        index += 1
+
+        # Get Discount
+        discount = float(lines[index])
+        index += 1
+
+    return grid_size, cashes, walls, reward, discount, prob, other_prob
 
 
 class MDPSolver:
     def __init__(self, input_file, max_time):
-        config = Config(input_file)
-        self.action_space = {
-            'U': {
-                'cost': config.MovementReward,
-                'dir_row': -1,
-                'dir_col': 0,
-                'p': config.ProbabilityOfMove,
-                'pretty': u'\u2191',
-                'other':
-                    [{
-                        'dir_row': -1,
-                        'dir_col': -1,
-                        'p': config.ProbabilityOfOtherMove
-                    },
-                        {
-                            'dir_row': -1,
-                            'dir_col': 1,
-                            'p': config.ProbabilityOfOtherMove
-                        }]
-            },
-            'D': {
-                'cost': config.MovementReward,
-                'dir_row': 1,
-                'dir_col': 0,
-                'p': config.ProbabilityOfMove,
-                'pretty': u'\u2193',
-                'other':
-                    [{
-                        'dir_row': 1,
-                        'dir_col': 1,
-                        'p': config.ProbabilityOfOtherMove
-                    },
-                        {
-                            'dir_row': 1,
-                            'dir_col': -1,
-                            'p': config.ProbabilityOfOtherMove
-                        }]
-            },
-            'L': {
-                'cost': config.MovementReward,
-                'dir_row': 0,
-                'dir_col': -1,
-                'p': config.ProbabilityOfMove,
-                'pretty': u'\u2190',
-                'other':
-                    [{
-                        'dir_row': 1,
-                        'dir_col': -1,
-                        'p': config.ProbabilityOfOtherMove
-                    },
-                        {
-                            'dir_row': -1,
-                            'dir_col': -1,
-                            'p': config.ProbabilityOfOtherMove
-                        }]
-            },
-            'R': {
-                'cost': config.MovementReward,
-                'dir_row': 0,
-                'dir_col': 1,
-                'p': config.ProbabilityOfMove,
-                'pretty': u'\u2192',
-                'other':
-                    [{
-                        'dir_row': -1,
-                        'dir_col': 1,
-                        'p': config.ProbabilityOfOtherMove
-                    },
-                        {
-                            'dir_row': 1,
-                            'dir_col': 1,
-                            'p': config.ProbabilityOfOtherMove
-                        }]
-            },
-            'E': {
-                'cost': 0,
-                'dir_row': 0,
-                'dir_col': 0,
-                'p': 1.0,
-                'pretty': '$',
-                'other': []
-            },
-            'N': {
-                'cost': 0,
-                'dir_row': 0,
-                'dir_col': 0,
-                'p': 1.0,
-                'pretty': 'X',
-                'other': []
-            }
-        }
+        self.start_time = time.time()
+        self.end_time = self.start_time + max_time
 
-        self.performable_actions = ['U', 'D', 'R', 'L']
+        self.grid_size, self.cashes, self.walls, reward, discount, prob, other_prob = read_file(input_file)
 
-        self.grid_size = config.GridSize
-        boi = config.BlockOfInterest
-        self.cash = config.CashGrids
-        self.movement_rewards = config.MovementReward
-        self.discount = config.Discount
-
-        self.boardA = dict()
-        self.boardB = dict()
-
-        self.policy = dict()
+        board = dict()
 
         for row in xrange(self.grid_size):
             for col in xrange(self.grid_size):
                 key = (row, col)
-                self.policy[key] = 'N'
-                if key not in boi:
-                    self.boardA[key] = 0.0
-                    self.boardB[key] = 0.0
-                else:
-                    if key in self.cash:
-                        self.policy[key] = 'E'
+                if key not in self.cashes and key not in self.walls:
+                    board[key] = Location(row, col, prob, other_prob, reward, discount)
 
-        self.end_time = time.time() + max_time
+        # Cache locations
+        for key, location in board.iteritems():
+            r, c = key
 
-    def get_reward_and_value(self, r, c, move):
-        action = self.action_space[move]
-        board = self.boardA
-        cash = self.cash
+            # Up
+            nr = r - 1
+            nc = c
+            location.actions[0][0] = board.get((nr, nc), self.cashes.get((nr, nc), board[key])).utility
 
-        nr = r + action['dir_row']
-        nc = c + action['dir_col']
-        prob = action['p']
-        utility_val = prob * board.get((nr, nc),
-                                       cash.get((nr, nc),
-                                                board[r, c]))
+            nr = r - 1
+            nc = c - 1
+            location.actions[0][1] = board.get((nr, nc), self.cashes.get((nr, nc), board[key])).utility
 
-        other = action['other'][0]
-        nr = r + other['dir_row']
-        nc = c + other['dir_col']
-        prob = other['p']
-        utility_val += prob * board.get((nr, nc),
-                                        cash.get((nr, nc),
-                                                 board[r, c]))
+            nr = r - 1
+            nc = c + 1
+            location.actions[0][2] = board.get((nr, nc), self.cashes.get((nr, nc), board[key])).utility
 
-        other = action['other'][1]
-        nr = r + other['dir_row']
-        nc = c + other['dir_col']
-        prob = other['p']
-        utility_val += prob * board.get((nr, nc),
-                                        cash.get((nr, nc),
-                                                 board[r, c]))
+            # Down
+            nr = r + 1
+            nc = c
+            location.actions[1][0] = board.get((nr, nc), self.cashes.get((nr, nc), board[key])).utility
 
-        return utility_val
+            nr = r + 1
+            nc = c - 1
+            location.actions[1][1] = board.get((nr, nc), self.cashes.get((nr, nc), board[key])).utility
 
-    def update(self, row, col):
-        reward_values = [(self.get_reward_and_value(row, col, move), move) for move in self.performable_actions]
-        max_reward, max_reward_dir = max(reward_values)
+            nr = r + 1
+            nc = c + 1
+            location.actions[1][2] = board.get((nr, nc), self.cashes.get((nr, nc), board[key])).utility
 
-        computed_reward = self.movement_rewards + self.discount * max_reward
-        return computed_reward, max_reward_dir
+            # Left
+            nr = r
+            nc = c - 1
+            location.actions[2][0] = board.get((nr, nc), self.cashes.get((nr, nc), board[key])).utility
 
-    def do_iteration(self):
-        for key, value in self.boardA.iteritems():
-            row, col = key
-            new_value, action = self.update(row, col)
-            self.policy[key] = action
-            self.boardB[key] = new_value
+            nr = r - 1
+            nc = c - 1
+            location.actions[2][1] = board.get((nr, nc), self.cashes.get((nr, nc), board[key])).utility
+
+            nr = r + 1
+            nc = c - 1
+            location.actions[2][2] = board.get((nr, nc), self.cashes.get((nr, nc), board[key])).utility
+
+            # Right
+            nr = r
+            nc = c + 1
+            location.actions[3][0] = board.get((nr, nc), self.cashes.get((nr, nc), board[key])).utility
+
+            nr = r - 1
+            nc = c + 1
+            location.actions[3][1] = board.get((nr, nc), self.cashes.get((nr, nc), board[key])).utility
+
+            nr = r + 1
+            nc = c + 1
+            location.actions[3][2] = board.get((nr, nc), self.cashes.get((nr, nc), board[key])).utility
+
+        self.resting_board = board
+        self.updating_board = list(board.values())
 
     def solve(self):
-        #iterations = 0
-        #sum_time = 0.0
+        iterations = 0
+        sum_time = 0.0
+
+        bit = False
+        anti = not bit
         while time.time() <= self.end_time:
-            #start = time.time()
-            self.do_iteration()
-            self.boardA, self.boardB = self.boardB, self.boardA
-            #end = time.time()
+            start = time.time()
 
-            #print('Took {}'.format(end - start))
-            #sum_time += (end - start)
-            #iterations += 1
+            for location in self.updating_board:
+                location.do_update(bit, anti)
 
-        #print('Average: {}'.format(sum_time / iterations))
+            bit = not bit
+            anti = not bit
+
+            end = time.time()
+
+            print('Took {}'.format(end - start))
+
+            sum_time += (end - start)
+            iterations += 1
+
+        print('Average: {} over {} iterations'.format(sum_time / iterations, iterations))
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
+        pretty_dict = {
+            'U': u'\u2191',
+            'D': u'\u2193',
+            'L': u'\u2190',
+            'R': u'\u2192',
+            'N': 'X',
+            'E': '$'
+        }
         full_str = ''
         for row in xrange(self.grid_size):
             row_str = ''
             for col in xrange(self.grid_size):
-                action = self.policy[(row, col)]
-                row_str += self.action_space[action]['pretty'] + ' '
+                key = (row, col)
+                action = self.resting_board.get(key, self.cashes.get(key, self.walls.get(key, None))).policy
+                row_str += pretty_dict[action] + ' '
 
             full_str += row_str + '\n'
 
@@ -250,8 +236,9 @@ class MDPSolver:
         for row in xrange(self.grid_size):
             row_str = ''
             for col in xrange(self.grid_size):
-                action = self.policy[(row, col)]
-                row_str += str(action) + ','
+                key = (row, col)
+                action = self.resting_board.get(key, self.cashes.get(key, self.walls.get(key, None))).policy
+                row_str += action + ','
             row_str = row_str.rstrip(',')
 
             full_str += row_str + '\n'
@@ -263,11 +250,11 @@ class MDPSolver:
 
 
 def main():
-    solver = MDPSolver('input3', 25.0)
+    solver = MDPSolver('input5', 28.0)
 
     solver.solve()
-    solver.write_out('output3')
-    #print(unicode(solver))
+    solver.write_out('output5')
+    print(unicode(solver))
 
 
 if __name__ == '__main__':
